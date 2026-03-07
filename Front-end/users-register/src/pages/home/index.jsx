a33b49f1ef6fce6704a782ee77db09e1951cdaff
Guilherme de Lima Ficagna <guilhermedelima925@gmail.com>
2026-03-07 19:42:59 -0300

    feat: insert content in index.jsx

--- Front-end/users-register/src/pages/home/index.jsx
+++ Front-end/users-register/src/pages/home/index.jsx
@@ -1,9 +1,33 @@
 import './style.css'
-
+import Trash from '../../assets/trash.png'
 function Home() {
   return (
-    <div>
-      <h1>Teste</h1>
+    <div className='container'>
+      <form action="">
+        <h1>User Register</h1>
+
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" />
+
+        <label htmlFor="age">Age</label>
+        <input type="number" id="age" />
+
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" />
+      </form>
+
+      <div className='list'>
+        <div>
+          <p>Name: </p>
+          <p>Age: </p>
+          <p>Email: </p>
+        </div>
+
+        <button>
+          <img src={Trash} alt=""/>
+        </button>
+      </div>
+
     </div>
   )
 }
