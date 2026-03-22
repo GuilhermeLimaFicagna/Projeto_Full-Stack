13f87de52fa81db8a303e793cb5dfdcf00324783
Guilherme de Lima Ficagna <guilhermedelima925@gmail.com>
2026-03-22 18:04:51 -0300

    feat: users delete

--- Front-end/users-register/src/pages/home/index.jsx
+++ Front-end/users-register/src/pages/home/index.jsx
@@ -4,12 +4,35 @@ import Trash from '../../assets/trash.png'
 import api from '../../services/api'
 
 function Home() {
-  
-  const [users, setUsers] =  useState([])
+  // to list
+  const [users, setUsers] = useState([])
 
+  // to capture and create
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+
+  // functions
   async function GetUsers() {
     const usersApi = await api.get('/users');
     setUsers(usersApi.data)
+  }
+
+  async function postUsers() {
+    await api.post('/users', {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value
+    })
+
+    GetUsers()
+  }
+
+  async function DeleteUsers(id) {
+    await api.delete(`/users/${id}`)
+
+    GetUsers()
   }
 
   useEffect(() => {
@@ -21,11 +44,11 @@ function Home() {
     <div className='container'>
       <form action="">
         <h1>User Register</h1>
-        <input type="text" placeholder="Type your name" id="name" />
-        <input type="number" placeholder="Type your age" id="age" />
-        <input type="email" placeholder="Type your email" id="email" />
+        <input type="text" placeholder="Type your name" id="name" ref={inputName} />
+        <input type="number" placeholder="Type your age" id="age" ref={inputAge} />
+        <input type="email" placeholder="Type your email" id="email" ref={inputEmail} />
 
-        <button type='button'>register</button>
+        <button type='button' onClick={postUsers}>register</button>
       </form>
 
       {/* User List */}
@@ -37,7 +60,7 @@ function Home() {
             <p>Email: <span>{user.email}</span></p>
           </div>
 
-          <button>
+          <button onClick={() => DeleteUsers(user.id)}>
             <img src={Trash} alt="" />
           </button>
         </div>
